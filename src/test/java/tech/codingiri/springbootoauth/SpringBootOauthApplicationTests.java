7a57687887c796953fa0da1ab7a0d7207526677c
Manoj Rathi <manojrathi25071997@gmail.com>
2022-02-06 19:17:25 +0530

    intial setup from spring initialzr

---
+++ src/test/java/tech/codingiri/springbootoauth/SpringBootOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.codingiri.springbootoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

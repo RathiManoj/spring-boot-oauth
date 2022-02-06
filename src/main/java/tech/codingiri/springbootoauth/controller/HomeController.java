40ecfe2bb4c9c26081161f042a87fd4ea4dd7145
Manoj Rathi <manojrathi25071997@gmail.com>
2022-02-06 19:19:23 +0530

    Add index route mapping

---
+++ src/main/java/tech/codingiri/springbootoauth/controller/HomeController.java
@@ -0,0 +1,12 @@
+package tech.codingiri.springbootoauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+}
